c6b185434ef42416f2f5b5f8d842b36c39e859fe
marcogarganigo <marcogarga11@gmail.com>
2024-09-24 10:51:50 +0200

    custom charset and options destructuring

--- fastpass.js
+++ fastpass.js
@@ -1,56 +1,75 @@
 var fastpass = (function () {
-
-    // Private variables and methods
-    var charsets = {
+    const charsets = {
         lowercase: 'abcdefghijklmnopqrstuvwxyz',
         uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
         numbers: '0123456789',
         symbols: '!@#$%^&*()_+-=[]{}|;:,.<>?'
     };
 
-    function getRandomInt(min, max) {
-        var array = new Uint32Array(1);
+    const getRandomInt = (min, max) => {
+        const array = new Uint32Array(1);
         crypto.getRandomValues(array);
         return array[0] % (max - min + 1) + min;
-    }
+    };
 
-    function generateRandomString(length, charset) {
-        var result = '';
-        for (var i = 0; i < length; i++) {
-            var randomIndex = getRandomInt(0, charset.length - 1);
+    const generateRandomString = (length, charset) => {
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            const randomIndex = getRandomInt(0, charset.length - 1);
             result += charset.charAt(randomIndex);
         }
         return result;
-    }
+    };
 
     // Public API
     return {
-        generate: function (options) {
-            options = options || {};
-            var length = options.length || 12;
-            var charsetsToUse = options.charsets || ['lowercase', 'uppercase', 'numbers'];
-            var excludeSimilar = options.excludeSimilar || false;
-            var excludeAmbiguous = options.excludeAmbiguous || false;
+        /**
+         * Generate a random string based on provided options.
+         * @param {Object} options - Options for string generation.
+         * @param {number} [options.length=12] - Length of the generated string.
+         * @param {string[]} [options.charsets=['lowercase', 'uppercase', 'numbers']] - Character sets to include.
+         * @param {boolean} [options.excludeSimilar=false] - Exclude similar looking characters.
+         * @param {boolean} [options.excludeAmbiguous=false] - Exclude ambiguous characters.
+         * @param {string} [options.customCharset] - A custom character set to use.
+         * @returns {string} The generated random string.
+         */
+        generate: function (options = {}) {
+            const {
+                length = 12,
+                charsets: charsetsToUse = ['lowercase', 'uppercase', 'numbers'],
+                excludeSimilar = false,
+                excludeAmbiguous = false,
+                customCharset
+            } = options;
 
-            // Make sure at least one character set is selected
-            if (charsetsToUse.length === 0) {
-                throw new Error('At least one character set must be selected.');
+            // Validate character sets
+            if (charsetsToUse.length === 0 && !customCharset) {
+                throw new Error('At least one character set must be selected or a custom charset must be provided.');
             }
 
-            var charset = '';
-            for (var i = 0; i < charsetsToUse.length; i++) {
-                var setName = charsetsToUse[i];
-                if (charsets[setName]) {
-                    charset += charsets[setName];
+            // Build the charset
+            let charset = customCharset || '';
+            if (!customCharset) {
+                for (const setName of charsetsToUse) {
+                    if (charsets[setName]) {
+                        charset += charsets[setName];
+                    } else {
+                        throw new Error(`Invalid character set: ${setName}`);
+                    }
                 }
             }
 
+            // Apply exclusions
             if (excludeSimilar) {
                 charset = charset.replace(/[iloIO01]/g, '');
             }
 
             if (excludeAmbiguous) {
                 charset = charset.replace(/[{}()\[\]\/\\~<>;:.,'"\?]/g, '');
+            }
+
+            if (charset.length === 0) {
+                throw new Error('No valid characters available to generate a string.');
             }
 
             return generateRandomString(length, charset);
--- readme.md
+++ readme.md
@@ -1,40 +1,64 @@
 # FastPass.js
 
+![NPM Downloads](https://img.shields.io/npm/dm/fastpass.js)
+
+
 [![](https://data.jsdelivr.com/v1/package/npm/fastpass.js/badge)](https://www.jsdelivr.com/package/npm/fastpass.js)
 
+
 FastPass.js is a lightweight JavaScript library that allows you to generate strong and secure random passwords quickly and easily. Whether you need to create a new password for your online accounts or generate multiple passwords for your team, FastPass.js has you covered.
-
-
 
 ## Installation
 You can install FastPass.js via <a href="https://www.npmjs.com/package/fastpass.js">npm</a>:
 
-<pre>npm install fastpass.js</pre>
+```bash
+npm install fastpass.js
+```
 
 Alternatively, you can include the following script tag in your HTML file to load the library from <a href="https://www.jsdelivr.com/package/npm/fastpass.js">jsDelivr's CDN</a>:
 
-<pre><script src="https://cdn.jsdelivr.net/npm/fastpass.js/fastpass.min.js"></script></pre>
+```html
+<script src="https://cdn.jsdelivr.net/npm/fastpass.js/fastpass.min.js"></script>
+```
+
 
 ## Usage
 
-To generate a random password with FastPass.js, simply call the <b>fastpass.generate()</b> method with your desired options:
+To generate a random password with FastPass.js, simply call the fastpass.generate() method with your desired options:
 
-<pre>
+```javascript
 var defaultPassword = fastpass.generate();
-</pre>
+```
 
-<pre>
+```javascript
 var customPassword = fastpass.generate({
-        length: 16,
-        charsets: ['lowercase', 'uppercase', 'numbers', 'symbols'],
-        excludeSimilar: true,
-        excludeAmbiguous: true
-    });
-</pre>
+    length: 16,
+    charsets: ['lowercase', 'uppercase', 'numbers', 'symbols'],
+    excludeSimilar: true,
+    excludeAmbiguous: true
+});
+```
 
-<b>excludeSimilar</b> exclude characters that can be easily confused, such as <b>i l 1 O 0 etc.</b>
 
-<b>excludeAmbiguous</b> exclude characters that are ambiguous in certain fonts, such as <b>{} () [] \/ ~ <> ; : . , ' " ?</b>
+<h3>Options</h3>
+
+* <b>length</b>: (optional) The length of the generated password. Default is 12.
+* <b>charsets</b>: (optional) An array of character set names to include. Default is ['lowercase', 'uppercase', 'numbers'].
+* <b>excludeSimilar</b>: (optional) Exclude characters that can be easily confused, such as i, l, 1, O, 0, etc. Default is false.
+* <b>excludeAmbiguous</b>: (optional) Exclude characters that are ambiguous in certain fonts, such as {}, (), [], /, ~, <, >, ;, :, ., ', ", ?. Default is false.
+* <b>customCharset</b>: (optional) A custom string representing a character set to use. This allows you to define exactly which characters to include in the generated password.
+
+
+<h3>Example with Custom Charset</h3>
+
+You can also generate a password using a custom character set:
+
+```javascript
+var customCharsetPassword = fastpass.generate({
+    customCharset: 'abcXYZ123!@#'
+});
+```
+
 
 ## Contributions
 
@@ -43,4 +67,3 @@ Contributions are welcome and greatly appreciated! If you find a bug, have a fea
 ## License
 
 FastPass.js is licensed under the <a href="https://opensource.org/license/mit/">MIT License</a>. Feel free to use, modify, and distribute the library as you see fit.
-
